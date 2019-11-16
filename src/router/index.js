4b7aba3fbcf377e46e203ce63a6495c4ec217970
哇哈哈 <2533304969@qq.com>
2019-11-16 08:45:01 +0800

    建立文章发布and文章列表二级容器

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Layout from '../views/layout'
 import Login from '../views/login'
+import Home from '@/views/home'
+import Publish from '@/views/publish'
+import Article from '@/views/article'
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,7 +20,21 @@ const routes = [
   // 一级路由主页
   {
     path: '/layout',
-    component: Layout
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Home
+      },
+      {
+        path: '/publish',
+        component: Publish
+      },
+      {
+        path: '/article',
+        component: Article
+      }
+    ]
   }
   // {
   //   path: '/about',
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -7,36 +7,32 @@
       <el-row>
         <el-col>
           <el-menu
-          style="width:221px"
-          background-color="#353b4e"
+            style="width:221px"
+            background-color="#353b4e"
             text-color="#adafb5"
             active-text-color="#ffd04b"
             @open="handleOpen"
             @close="handleClose"
           >
-            <el-menu-item index="1">
+            <el-menu-item index="/home">
               <span slot="title">首页</span>
             </el-menu-item>
-            <el-submenu index="2">
-              <template slot="title">
-                <span>内容管理</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
-                <el-menu-item index="2-3">选项3</el-menu-item>
-                <el-menu-item index="2-4">选项3</el-menu-item>
-              </el-menu-item-group>
+            <el-submenu index="1">
+              <template slot="title">内容管理</template>
+              <el-menu-item index="/publish">发布文章</el-menu-item>
+              <el-menu-item index="/article">内容列表</el-menu-item>
+              <el-menu-item index="2-3">评论列表</el-menu-item>
+              <el-menu-item index="2-4">素材管理</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
               <template slot="title">
                 <span>内容管理</span>
               </template>
               <el-menu-item-group>
-                <el-menu-item index="3-1">选项1</el-menu-item>
-                <el-menu-item index="3-2">选项2</el-menu-item>
-                <el-menu-item index="3-3">选项3</el-menu-item>
-                <el-menu-item index="3-4">选项3</el-menu-item>
+                <el-menu-item index="3-1">图文数据</el-menu-item>
+                <el-menu-item index="3-2">粉丝概况</el-menu-item>
+                <el-menu-item index="3-3">粉丝画像</el-menu-item>
+                <el-menu-item index="3-4">粉丝列表</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <el-menu-item index="4">
@@ -57,7 +53,6 @@
       </el-main>
     </el-container>
   </el-container>
-
 </template>
 <script>
 import layoutHeader from '../../components/layout-header'
